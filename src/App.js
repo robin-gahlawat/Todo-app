244bc05e4b73a0b4fb7bdc1f9082ee84c6313c86
robin <robingahlawat30@gmail.com>
2021-07-18 22:47:15 +0530

    adding todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {v4 as uuidv4} from "uuid";
+import React, { useState, useEffect, useRef} from 'react';
+
+
 function App() {
+
+  const firstRender = useRef(true);
+  const [inputValue, setInputValue] = useState("");
+  const[tasks, setTasks] = useState([]);
+
+  const addTask = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim() === "") return;
+
+    setTasks([...tasks, {text: inputValue, id: uuidv4(),}, ]);
+    setInputValue("");
+  };
+
+  const removeTask = (id) =>{
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  useEffect(() => {
+
+    if(firstRender.current){
+      console.log('true');
+      firstRender.current = false; 
+    }
+    else{
+      localStorage.setItem("Task", JSON.stringify([...tasks]));
+      console.log("not first page load");
+    }
+  }, [tasks]);
+
+  useEffect(() => {
+      if(localStorage.getItem("Task") !== null) {
+        const newTasks = localStorage.getItem("Task");
+        setTasks(JSON.parse([...tasks, newTasks]));
+      }
+
+  }, []); 
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="container">
+
+        <form onSubmit={addTask}>
+          <input
+            type="text"
+            placeholder="Add a task..."
+            autoFocus
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+
+        {tasks.map((task) => (
+          <div key={task.id} className="task">
+            <p>{task.text}</p>
+            <i onClick={() => removeTask(task.id)} className="far fa-trash-alt"></i>
+          </div>
+         ))}
+
+      </div>
     </div>
   );
-}
+};
 
 export default App;
